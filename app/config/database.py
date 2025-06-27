b961779524c27984cd89a1bab4c4b89d6ff83e42
Francisco Mateus <fmfr@outlook.com>
2025-06-27 17:07:50 -0300

    ajustando aplicação containerizada para falar com banco em container

--- app/config/database.py
+++ app/config/database.py
@@ -5,9 +5,8 @@ import os
 # Para aplicação na máquina: 
 # DATABASE_URL = DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/spotter"
 
-# Para container:
-# host.docker.internal é o endereço para acessar o host local a partir de dentro do container no Docker Desktop (Windows, Mac e WSL2 no Linux).
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@host.docker.internal:5432/spotter_db")
+# Para aplicação e banco dentro de container:
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@postgres-spotter:5432/spotter")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
