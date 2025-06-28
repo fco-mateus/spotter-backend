49ff5b0e11bc7b98bc8470b255c40f8a88de5c74
Francisco Mateus <fmfr@outlook.com>
2025-06-27 23:31:02 -0300

    aplicação integrada com ocr rodando localmente

--- app/utils/convert_base64.py
+++ app/utils/convert_base64.py
@@ -3,4 +3,4 @@ import base64
 def converter_imagem_para_base64_formatado(file_content, mime_type):
     imagem_base64 = base64.b64encode(file_content).decode('utf-8')
     # Monta a string no formato esperado
-    return f"data:{mime_type};base64,/{imagem_base64}"
+    return f"data:{mime_type};base64,{imagem_base64}"
