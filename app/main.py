2cf92f3bab471f1cf8a9c4688e8dc5fcde4ea50b
Francisco Mateus <fmfr@outlook.com>
2025-07-04 00:32:06 -0300

    liberando CORS

--- app/main.py
+++ app/main.py
@@ -3,7 +3,17 @@ from app.controllers import ocorrencias_controller
 from app.controllers import veiculos_controller
 from app.controllers import funcionarios_controller
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ocorrencias_controller.router)
 app.include_router(veiculos_controller.router)
